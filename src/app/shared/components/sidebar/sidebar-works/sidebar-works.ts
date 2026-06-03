c0ac6479e31cfd0738cb012b30440fd5e1816e61
Ahmed Sami <ahnurmed16@gmail.com>
2026-06-03 15:46:17 +0300

    dark-mode hundelation

--- src/app/shared/components/sidebar/sidebar-works/sidebar-works.ts
+++ src/app/shared/components/sidebar/sidebar-works/sidebar-works.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, inject, Input, Output, signal } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output, signal } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 export interface NavItem {
@@ -15,7 +15,17 @@ export interface NavItem {
   templateUrl: './sidebar-works.html',
   styleUrl: './sidebar-works.css',
 })
-export class SidebarWorks {
+export class SidebarWorks  implements OnInit {
+  
+
+ngOnInit(): void {
+  const savedTheme = localStorage.getItem('theme');
+
+  if (savedTheme === 'dark') {
+    this.isDarkTheme.set(true);
+    document.body.classList.add('dark-theme');
+  }
+}
   @Input() isExpanded = true;
   @Output() toggleSidebar = new EventEmitter<void>();
 
@@ -23,9 +33,11 @@ export class SidebarWorks {
   router=inject(Router)
 
   mainNav: NavItem[] = [
-    { label: 'Dashboard', icon: 'ti-layout-dashboard',},
+    { label: 'Dashboard', icon: 'ti-layout-dashboard', routerLink: '/dashboard' },
     { label: 'Home', icon: 'ti-home', routerLink: '/home' },
-    { label: 'Help', icon: 'ti-help-circle' },
+    { label: 'Finger Print', icon: 'ti-fingerprint', routerLink: '/finger-print' },
+    { label: 'Finger  Device', icon: 'ti-device-imac', routerLink: '/finger-device' },
+    { label: 'Help', icon: 'ti-help-circle', routerLink: '/help' },
     { label: 'Login', icon: 'ti-login',routerLink: '/login' ,},
   ];
 
@@ -42,8 +54,17 @@ export class SidebarWorks {
 
   setTheme(dark: boolean): void {
     this.isDarkTheme.set(dark);
+     if (dark) {
+    document.body.classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.body.classList.remove('dark-theme');
+    localStorage.setItem('theme', 'light');
+  }
   }
   logout(){
      this.router.navigate(['/login'])
   }
+
+  
 }
