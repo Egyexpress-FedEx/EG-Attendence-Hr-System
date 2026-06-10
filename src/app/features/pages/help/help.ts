874bc12b3cad8ce324313a9d691d487f42d7b3a3
Ahmed Sami <ahnurmed16@gmail.com>
2026-06-10 12:38:56 +0300

    dash translate

--- src/app/features/pages/help/help.ts
+++ src/app/features/pages/help/help.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HelpComponent } from '../../../shared/components/pages/help/help';
+import { debounce } from 'rxjs';
 
 @Component({
   selector: 'app-help',
