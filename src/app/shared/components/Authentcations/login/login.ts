c7089148216d9adbe77887dd4cd25fb79aa4e6e8
Ahmed Sami <ahnurmed16@gmail.com>
2026-06-11 17:18:39 +0300

    clean code

--- src/app/shared/components/Authentcations/login/login.ts
+++ src/app/shared/components/Authentcations/login/login.ts
@@ -1,12 +1,9 @@
-import { Component, signal, OnInit, inject } from '@angular/core';
-import { form, required, email, FormField } from '@angular/forms/signals';
+import { Component,inject } from '@angular/core';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 import { loginser } from '../../../../core/services/auth/login';
 import { FormsModule} from '@angular/forms';
-import { SidebarWorks } from '../../sidebar/sidebar-works/sidebar-works';
-import { TranslateBtn } from '../../translate-btn/translate-btn';
-import { Themes } from '../../themes/themes';
+
 
 @Component({
   selector: 'app-login',
@@ -15,6 +12,7 @@ import { Themes } from '../../themes/themes';
   styleUrl: './login.css',
 })
 export class Login {
+  
 loginService = inject(loginser)
 router= inject(Router)
  email = '' ;
